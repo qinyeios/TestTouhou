dc1f73ff48a9a371bc6d9a2322b3a47ffa80a4bf
覃邺 <qinye@tanyedeimac.local>
2016-10-09 09:51:25 +0800

    Initial Commit

---
+++ TestTouhou/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestTouhou
+//
+//  Created by 覃邺 on 16/10/9.
+//  Copyright © 2016年 覃邺. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestTouhou/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  TestTouhou
+//
+
+//  Copyright (c) 2016年 覃邺. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SceneKit/SceneKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
